6ac82d9586daf27db51832dd47be949c8fdac94d
Jaler Dio Fadhilah <37465606+jalerdio@users.noreply.github.com>
2021-01-04 03:11:59 +0700

    Update shoot.js

--- shoot.js
+++ shoot.js
@@ -27,3 +27,20 @@ function getDirection(camera, speed) {
 
   return { x: moveX * speed, y: moveY * speed, z: -moveZ * speed };
 }
+
+const shoot = () => {
+  const bullet = document.createElement("a-sphere");
+  let pos = myCamera.getAttribute("position");
+  bullet.setAttribute("position", pos);
+  bullet.setAttribute("velocity", getDirection(myCamera, 30));
+  bullet.setAttribute("dynamic-body", true);
+  bullet.setAttribute("radius", 0.5);
+  bullet.setAttribute("src", "textures/ball.jpg");
+  myScene.appendChild(bullet);
+};
+
+document.onkeydown = event => {
+  if (event.which == 32) {
+    shoot();
+  }
+};
